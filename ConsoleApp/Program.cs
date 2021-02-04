3dd5f3d8cec8537b426dad48e8c6b954b0051e50
Benjamin Michaelis <benjamin@michaelis.net>
2021-02-03 17:57:41 -0800

    Enable Stylecop on all solutions with single .json and cross-build
    - Turned on stylecop for all solutions and made stylecop.json accessible from the solution items to each individual project
    - Allowed the console app and tests to cross build framework and net5.0

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,12 @@
-﻿// <copyright file="Program.cs" company="Benjamin Michaelis">
-// Copyright (c) Benjamin Michaelis. All rights reserved.
-// </copyright>
+﻿using System;
 
 namespace ConsoleApp
 {
-    using System;
-
-    internal class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Press Enter Key to Exit.");
-            Console.ReadLine();
         }
     }
 }
