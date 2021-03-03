d90ab7ad29f748b06b27aa2d2a2c0c8b061706a2
Benjamin Michaelis <benjamin@michaelis.net>
2021-03-03 11:09:24 -0800

    Add Spreadsheet Engine Project

---
+++ SpreadsheetEngine/SpreadsheetCell.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="SpreadsheetCell.cs" company="Benjamin Michaelis">
+// Copyright (c) Benjamin Michaelis. ID: 11620581. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace SpreadsheetEngine
+{
+    /// <summary>
+    /// Represents one cell in the worksheet.
+    /// </summary>
+    public class SpreadsheetCell
+    {
+    }
+}
