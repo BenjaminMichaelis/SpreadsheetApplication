6bbe6f10fe5b8b497b9caecf58f1a75325c4578c
Benjamin Michaelis <benjamin@michaelis.net>
2021-03-03 11:51:56 -0800

    Add RowIndex and ColumnIndex to SpreadsheetCell
    - add these two read-only properties with their field in abstract class SpreadsheetCell

--- SpreadsheetEngine/SpreadsheetCell.cs
+++ SpreadsheetEngine/SpreadsheetCell.cs
@@ -9,7 +9,37 @@ namespace SpreadsheetEngine
     /// <summary>
     /// Represents one cell in the worksheet.
     /// </summary>
-    public class SpreadsheetCell
+    public abstract class SpreadsheetCell
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpreadsheetCell"/> class.
+        /// </summary>
+        /// <param name="columnIndex">sets the readonly columnIndex.</param>
+        /// <param name="rowIndex">sets the readonly rowIndex.</param>
+        public SpreadsheetCell(int rowIndex, int columnIndex)
+        {
+            this._rowIndex = rowIndex;
+            this._columnIndex = columnIndex;
+        }
+
+        private readonly int _rowIndex;
+
+        /// <summary>
+        /// Gets (read-only) row index.
+        /// </summary>
+        public int RowIndex
+        {
+            get { return this._rowIndex; }
+        }
+
+        private readonly int _columnIndex;
+
+        /// <summary>
+        /// Gets (read-only) column index.
+        /// </summary>
+        public int ColumnIndex
+        {
+            get { return this._columnIndex; }
+        }
     }
 }
