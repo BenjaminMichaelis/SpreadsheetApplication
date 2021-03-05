61208a02b5623f1d5e83a801ca0e60731cfd1427
Benjamin Michaelis <benjamin@michaelis.net>
2021-03-04 20:30:40 -0800

    Add 2D Spreadsheet cells array

---
+++ SpreadsheetEngine/Spreadsheet.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Spreadsheet.cs" company="Benjamin Michaelis">
+// Copyright (c) Benjamin Michaelis. ID: 11620581. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine
+{
+    /// <summary>
+    /// Is a container of a 2D array of cells.
+    /// </summary>
+    public class Spreadsheet
+    {
+        private SpreadsheetCell[,] _cellsOfSpreadsheet;
+
+        /// <summary>
+        /// Initializes spreadsheet with rows and columns of cells.
+        /// </summary>
+        /// <param name="rows">Number of rows.</param>
+        /// <param name="columns">Number of columns.</param>
+        public Spreadsheet(int rows, int columns)
+        {
+            this._cellsOfSpreadsheet = new SpreadsheetCell[rows, columns];
+        }
+    }
+}
