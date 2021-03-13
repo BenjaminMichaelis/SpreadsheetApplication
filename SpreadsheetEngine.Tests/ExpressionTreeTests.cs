02e968d7171657322d0233eeb392956ba51343c9
Benjamin Michaelis <benjamin@michaelis.net>
2021-03-12 20:12:32 -0800

    TDD Create test

--- SpreadsheetEngine.Tests/ExpressionTreeTests.cs
+++ SpreadsheetEngine.Tests/ExpressionTreeTests.cs
@@ -3,6 +3,8 @@
 // </copyright>
 
 using System;
+using System.Linq;
+using System.Text;
 using CptS321;
 using Xunit;
 
@@ -36,12 +38,37 @@ namespace SpreadsheetEngine.Tests
         // [InlineData("2*3+5", 11.0)]
         // [InlineData("2+3*5", 17.0)]
         // [InlineData("2 + 3 * 5", 17.0)]
-        [InlineData("A-B-C", 0)]
-        [InlineData("3+5", 8.0)]
-        public void EvaluateTests(string expression, double expected)
+        // [InlineData("A-B-C", 0)]
+        // [InlineData("3+5", 8.0)]
+        [InlineData("A", "0", 0)]
+        [InlineData("A", "5", 5)]
+        [InlineData("A+B", "10+100", 110)]
+        public void EvaluateTests(string expression, string values, double expected)
         {
             ExpressionTree tree = new ExpressionTree(expression);
+            foreach (int value in ExpressionTree.GetSymbols(values).Where(item => !ExpressionTree.IsOperator(item))
+                .Select(item => int.Parse(item)))
+            {
+                tree.SetVariable(expression, value);
+            }
             Assert.Equal(expected, tree.Evaluate());
+        }
+
+        /// <summary>
+        /// Test get symbols function.
+        /// </summary>
+        /// <param name="expression">Pass in expression to test.</param>
+        [Theory]
+        [InlineData("A+B+C")]
+        public void GetSymbols(string expression)
+        {
+            StringBuilder result = new (string.Empty);
+            foreach(string symbol in ExpressionTree.GetSymbols(expression))
+            {
+                result.Append(symbol);
+            }
+
+            Assert.Equal(expression, result.ToString());
         }
     }
 }
