dda12412dcc21a6f9f4f38c637e1bd52d9389d4e
Benjamin Michaelis <benjamin@michaelis.net>
2022-07-28 22:01:01 -0700

    Add clowdSquirrel

--- Spreadsheet/Program.cs
+++ Spreadsheet/Program.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Benjamin Michaelis. All rights reserved.
 // </copyright>
 
+using Squirrel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,10 @@ namespace SpreadsheetApp
         [STAThread]
         public static void Main()
         {
+            SquirrelAwareApp.HandleEvents(
+        onInitialInstall: OnAppInstall,
+        onAppUninstall: OnAppUninstall,
+        onEveryRun: OnAppRun);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Initialize();
@@ -45,6 +50,22 @@ namespace SpreadsheetApp
             MainViewModel viewModel = new(mainForm);
 
             Application.Run(mainForm);
+        }
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
         }
     }
 }
